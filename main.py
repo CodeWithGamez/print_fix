745114053ea1a017d6c1e63841c6947cb11fdf08
CodeWithGamez <codewithgamez@gmail.com>
2020-08-16 22:21:31 +0000

    Adding what I want to be printed

---
+++ main.py
@@ -0,0 +1,19 @@
+#Fix my mistakes of using PRINT
+
+
+#1 I want this to print hello to console
+print("hello"
+
+
+
+#2 I want this to print four to the console
+print(four)
+
+
+
+
+#3 I want this to print This should work to the console
+prnt("This should work")
+
+
+
